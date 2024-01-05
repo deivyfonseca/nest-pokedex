5d975d435b7a2b7a77d6af5883ef3837f230bb21
deivfobe <deivfobe@suramericana.com.co>
2024-01-04 20:24:07 -0500

    Fin

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,14 +5,22 @@ import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
+  ){
+    // console.log( process.env.DEFAULT_LIMIT );
+    // console.log( configService.get<number>('default_limit') );
+    this.defaultLimit = configService.get<number>('default_limit');
+  }
 
   /**
   * Create a pokemon
@@ -33,7 +41,7 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto ) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
